b5c53117f9c2273971b406866d978d1fa41a66c5
BeatriceDuncan <bealmduncan@gmail.com>
2021-03-13 16:56:02 -0500

    finished submit & change functions, now communicating with database

--- src/App.js
+++ src/App.js
@@ -1,42 +1,54 @@
-import firebase from './firebase';
-import './App.css';
-import React, { useState, useEffect, } from 'react';
+import firebase from "./firebase";
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
-
-  const [promptArray, setPromptArray] = useState([])
-
+  const [promptArray, setPromptArray] = useState([]);
+  const [textInput, setTextInput] = useState("");
 
   useEffect(() => {
-
-    const dbRef = firebase.database().ref()
-    dbRef.on('value', (data) => {
-      // console.log(data.val());
+    const dbRef = firebase.database().ref();
+    dbRef.on("value", (data) => {
       const promptData = data.val();
-      console.log(promptData);
+      // console.log(promptData);
 
       const promptItems = [];
 
       for (let promptKey in promptData) {
-        promptArray.push(
-          promptData[promptKey]
-        );
-      };
+        promptArray.push({
+          uniqueKey: promptKey,
+          userPrompt: promptData[promptKey],
+        });
+      }
 
       setPromptArray(promptData);
     });
+  }, []);
 
-  }, [])
+  const handleChange = (event) => {
+    setTextInput(event.target.value);
+  };
 
-
-
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const dbRef = firebase.database().ref();
+    dbRef.push(textInput);
+    setTextInput("");
+  };
 
   return (
     <div className="App">
-      <h1>4 U 2 Write</h1>
+      <h1>Write On</h1>
       <form action="" onSubmit={handleSubmit}>
-        <label htmlFor="promptSubmit" >Submit a writing prompt of your own!</label>
-        <input type="text" id="promptSubmit" onChange={handleChange} value={textInput}/>
+        <label htmlFor="promptSubmit">
+          Submit a writing prompt of your own!
+        </label>
+        <input
+          type="text"
+          id="promptSubmit"
+          onChange={handleChange}
+          value={textInput}
+        />
         <button>Add your prompt!</button>
       </form>
     </div>
