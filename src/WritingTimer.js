5e5ed0a28433d8a756e65672f1cd9661ba17f664
BeatriceDuncan <bealmduncan@gmail.com>
2021-03-15 19:26:25 -0400

    solved writingTimer issues, now passing user select time to countdown timer

--- src/WritingTimer.js
+++ src/WritingTimer.js
@@ -1,6 +1,5 @@
-import { CountdownCircleTimer } from "react-countdown-circle-timer";
 import { useState } from "react";
-import "./writingTimer.css";
+import ReactCountdownClock from 'react-countdown-clock';
 
 function WritingTimer() {
   const [timeGoal, setTimeGoal] = useState(0);
@@ -9,41 +8,34 @@ function WritingTimer() {
     setTimeGoal(parseInt(event.target.value));
   };
 
-  // const timesUp = ({ remainingTime }) => {
-  //   if (remainingTime === 0) {
-  //     return <div className="timesUp">You met your writing goal!</div>;
-  //   }
-  // };
-
   return (
     <div className="writingTimer">
-        <select
-          className="timeSelect"
-          name="timeSelect"
-          id="timeSelect"
-          aria-label="choose how long you want to write for"
-          onChange={handleTimeSelect}
-          value={timeGoal}
-        >
-          <option value="" disabled selected>select time goal</option>
-          <option value="600">10 mins</option>
-          <option value="900">15 mins</option>
-          <option value="1200">20 mins</option>
-          <option value="1800">30 mins</option>
-          <option value="2700">45 mins</option>
-        </select>
-      <CountdownCircleTimer
-        // initialRemainingTime={timeGoal}
-        size={75}
-        trailStrokeWidth={25}
-        strokeWidth={25}
-        isPlaying
-        duration={10} //timeGoal state needs to go here instead of 600
-        colors={[["#78cad2", 0.33], ["#50869a", 0.33], ["#23395b"]]}
-        onComplete={() => [false]}
+      <select
+        className="timeSelect"
+        name="timeSelect"
+        id="timeSelect"
+        aria-label="choose how long you want to write for"
+        onChange={handleTimeSelect}
+        value={timeGoal}
       >
-      {/* {timesUp} */}
-      </CountdownCircleTimer>
+        <option>select time goal</option>
+        <option value="10">10 seconds</option>
+        <option value="600">10 mins</option>
+        <option value="900">15 mins</option>
+        <option value="1200">20 mins</option>
+        <option value="1800">30 mins</option>
+        <option value="2700">45 mins</option>
+      </select>
+      <ReactCountdownClock
+        seconds={timeGoal}
+        showMilliseconds={false}
+        // font={}
+        color="#78cad2"
+        alpha={0.9}
+        size={75}
+        weight={15}
+        // handleComplete={takes a function}
+      />
     </div>
   );
 }
