cf80e45d74e77b54114238808677c8509857b890
amy <amy.bradford3@gmail.com>
2021-03-16 13:54:03 +0000

    worked on daily prompt generator

--- src/App.js
+++ src/App.js
@@ -8,26 +8,47 @@ import PromptSchedule from "./PromptSchedule.js";
 
 function App() {
   const [promptArray, setPromptArray] = useState([]);
-  const [textInput, setTextInput] = useState("");
+  const [textInput, setTextInput] = useState("");  
 
   useEffect(() => {
     const dbRef = firebase.database().ref();
     dbRef.on("value", (data) => {
       const promptData = data.val();
-      // console.log(promptData);
-
+      
       const promptItems = [];
-
       for (let promptKey in promptData) {
         promptArray.push({
           uniqueKey: promptKey,
           userPrompt: promptData[promptKey],
         });
       }
-
       setPromptArray(promptData);
     });
   }, []);
+  //create state for dailyPrompt and randomPrompt
+  const [dailyPrompt, setDailyPrompt] = useState([]);
+  const [randomPrompt, setRandomPrompt] = useState(0);
+  // create random prompt function
+  // pass a parameter so we can have access to other info
+  const randomDailyPrompt = (maxNumberOfPrompts) => {
+    return Math.floor(Math.random() * maxNumberOfPrompts);
+  }
+  console.log('list of all values in prompt array', Object.values(promptArray));
+  // create function to generate the prompt on the page
+  // need to get access to the length of the number of prompts
+  const generatePrompt = () => {
+    const maxNumberOfPrompts = Object.values(promptArray).length;
+    console.log(maxNumberOfPrompts);
+    // conditions so that same prompt isn't shown twice in a row
+    let newDailyPrompt = randomDailyPrompt(maxNumberOfPrompts);
+    while (newDailyPrompt === randomDailyPrompt) {
+      newDailyPrompt = randomDailyPrompt(maxNumberOfPrompts);    
+  }
+    setRandomPrompt(newDailyPrompt);
+    const randomKey = (promptArray)[randomPrompt];
+    const todaysPrompt = (promptArray[randomKey]);
+    setDailyPrompt(todaysPrompt);    
+  }
 
   const handleChange = (event) => {
     setTextInput(event.target.value);
@@ -43,6 +64,10 @@ function App() {
   return (
     <div className="App">
       <Header />
+      {/* added test area to generate prompt */}
+      <div>
+        <p>{}</p>
+      </div>
       <WritingArea />
       <UserPrompt
         submit={handleSubmit}
--- src/PromptSchedule.js
+++ src/PromptSchedule.js
@@ -1,30 +1,23 @@
-import { useState, useEffect } from "react";
-// this gives access to timezone/date of user - maybe we try to use this data to link to a daily change
-let newDate = new Date();
-  console.log(newDate);
-
-// create a function to change prompt every 3 seconds (to test out timer)
+import { useState, useEffect } from 'react';
+// NEW SOLUTION
+// create new object that contains two properties: dateCreated and index
+// save them in firebase
+// when user enters the page, test if dateCreated = today
+// if not, add 1 (test for maximum size) to the index and change dateCreated to today
+// save on firebase
+// retrieve the value inside database index
 function PromptSchedule() {
-  // need to access firebase data array, for now have created my own array
-  let prompt = ["love", "spring", "aliens"];  
-  // create state for time in seconds
-  const [seconds, setSeconds] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setSeconds(second => (second === 2 ? 0 : second + 1))
-    }, 3000);
-    // end of interval, restart from beginning
-    return () => clearInterval(interval);
-    }, [seconds]);
+  
+  
 
   return (
     <div>
-      <p>{prompt[seconds]}</p>
+      <p></p>
     </div>
   )
-};
-
+}
+  
 export default PromptSchedule;
 
-// TIPS - if dateDay !=== dateDay then change to new prompt
+
+
