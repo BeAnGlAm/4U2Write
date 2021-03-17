1624bf443259867f58b8e9ee84b26051f450935a
andrewcampbell <awr.campbell@gmail.com>
2021-03-17 16:28:17 -0400

    added scss styling to header and app.scss

--- src/Header.js
+++ src/Header.js
@@ -10,14 +10,16 @@ function Header() {
 
    return (
       <header className={ showFull ? "header-full" : "header-hidden" } >
-         <p className="wrapper">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem ipsam deserunt eos recusandae odio sequi eligendi quis iusto accusamus iure. Eos illum possimus reiciendis quam maxime vel perferendis beatae laudantium, veritatis, distinctio fuga. Asperiores consectetur voluptates excepturi sequi quos sint, unde ipsum aperiam fuga veniam ea, corrupti, tenetur beatae quae?</p>
-         <nav className="wrapper">
-            <h1>Write On</h1>
-            <div>
-               <h3>Learn more</h3>
-               {/* Arrow icon */}
-            </div>
-         </nav>
+         <div className="wrapper">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem ipsam deserunt eos recusandae odio sequi eligendi quis iusto accusamus iure. Eos illum possimus reiciendis quam maxime vel perferendis beatae laudantium, veritatis, distinctio fuga. Asperiores consectetur voluptates excepturi sequi quos sint, unde ipsum aperiam fuga veniam ea, corrupti, tenetur beatae quae?</p>
+            <nav>
+               <h1>Write On</h1>
+               <div>
+                  <h3>Learn more</h3>
+                  {/* Arrow icon */}
+               </div>
+            </nav>
+         </div>
       </header>
    )
 }
