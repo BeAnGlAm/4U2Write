f3dd181a1b19a3152e03fb6e2fb4d1715b62df64
BeatriceDuncan <bealmduncan@gmail.com>
2021-03-18 19:56:55 -0400

    added an about component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import WritingArea from "./components/WritingArea.js";
 import IdleTimer from './components/IdleTimer.js';
 import PromptSubmit from "./components/PromptSubmit";
 import UserPrompt from "./components/UserPrompt";
+import About from "./components/About";
 import Footer from './components/Footer.js';
 
 import Swal from 'sweetalert2';  // can potentially be moved with handleSubmit and handleChange later? 
@@ -163,6 +164,7 @@ function App() {
           change={handleChange}
           input={textInput}
         />}
+        <About />
         <Footer />
     </div>
   );
