9f613417859d31c52789162610ca44fbbc012925
amy <amy.bradford3@gmail.com>
2021-03-18 23:05:52 +0000

    removed p tag on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,6 @@ function Footer() {
           Created by BeAnGlAm @{" "}
           <a href="https://junocollege.com">Juno College</a>
         </p>
-        <p>Amy Bradford | Andrew Campbell | Bea Duncan | Glen San Jose</p>
         <p>
           Initial writing prompts courtesy of{" "}
           <a href="https://blog.prepscholar.com/creative-writing-prompts#:~:text=%231%3A%20Include%20something%20falling%20in,my%20imagination%20begins%20like%20this%3A%22">
